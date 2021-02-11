f724ba3b28c10c77dda295dbb07fa466cbfbb596
gbuccir <gbucci@uol.com.br>
2021-02-10 23:34:34 -0300

    express path

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
+//Install express server
 const express = require('express');
+// Requirement of path Module in order to manage URLS
+const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist'));
-app.all('*', (req, res) => {
-  res.status(200).sendFile(__dirname + '/src/index.html');
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/my-app'));
+app.get('/*', function(req,res) {
+   res.sendFile(path.join(__dirname+'/dist/my-app/index.html'));
 });
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
